a9a4a206a7f179d2e160e0f116f89f43b7482b86
João Marcos Batista Nunes <134300635+joaomarcosbn@users.noreply.github.com>
2025-06-17 16:22:53 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,109 +1,34 @@
-class Pokemon {
-    constructor() {
-        this.number = '';
-        this.name = '';
-        this.type = '';
-        this.types = [];
-        this.photo = '';
-        this.height = '';
-        this.weight = '';
-        this.abilities = [];
-        this.stats = [];
-    }
-}
-
-// Exporte a classe para que ela possa ser usada em outros arquivos
-export default Pokemon;
-
-const pokeApi = {}
+const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
 
-    pokemon.types = types
-    pokemon.type = type
+  pokemon.types = types;
+  pokemon.type = type;
 
-    const abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
-    pokemon.abilities = abilities
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
 
-    const stats = pokeDetail.stats.map((statSlot) => ({
-        name: statSlot.stat.name,
-        baseStat: statSlot.base_stat
-    }))
-    pokemon.stats = stats
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
+  return pokemon;
 }
 
-// Função para buscar os detalhes do Pokémon com base no nome
-pokeApi.getPokemonDetails = (name) => {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Pokémon não encontrado");
-            }
-            return response.json();
-        })
-        .then(convertPokeApiDetailToPokemon);
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
 };
 
-export function displayPokemon(pokemon) {
-    const pokemonDetailContainer = document.getElementById('pokemon-detail-container');
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-    pokemonDetailContainer.innerHTML = `
-        <div class="${pokemon.type}">
-            <div class="header">
-                <h1>${pokemon.name}</h1>
-                <span class="number">#${pokemon.number}</span>
-            </div>
-            <div class="image-container">
-                <img src="${pokemon.photo}" alt="${pokemon.name}">
-            </div>
-            <div class="container">
-                <div class="tags">
-                    ${pokemon.types.map((type) => `<span class="${type}">${type}</span>`).join('')}
-                </div>
-                <div class="about">
-                    <h2>About</h2>
-                    <div class="details">
-                        <div>
-                            <p><strong>${pokemon.weight} kg</strong></p>
-                            <p>Weight</p>
-                        </div>
-                        <div>
-                            <p><strong>${pokemon.height} m</strong></p>
-                            <p>Height</p>
-                        </div>
-                        <div>
-                            ${pokemon.abilities.map((ability) => `<p><strong>${ability}</strong></p>`).join('')}
-                            <p>Abilities</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="base-stats">
-                    <h2>Base Stats</h2>
-                    <div class="stats">
-                        ${pokemon.stats.map((stat) => `
-                            <div class="stat">
-                                <span class="label">${stat.name}</span>
-                                <span class="divider"></span>
-                                <span>${stat.baseStat}</span>
-                                <progress value="${stat.baseStat}" max="100" class="progressBar"></progress>
-                            </div>
-                        `).join('')}
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
-}
-
-// Exporte o objeto pokeApi para ser usado em outros arquivos
-export { pokeApi };
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails);
+};
